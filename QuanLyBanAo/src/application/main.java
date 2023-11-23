4bc87a8fab689d3c64d51fc081df1a22ef8215fc
DESKTOP-SBB065O\toten <duchuy22x@gmail.com>
2023-11-23 21:22:04 +0700

    them application

---
+++ QuanLyBanAo/src/application/main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+import views.frm_Login;
+
+public class main {
+
+    public static void main(String[] args) {
+        new frm_Login().setVisible(true);
+    }
+}
